63fbcab9293699b139ceed21865a2ede5252218c
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-02-07 12:04:35 +0530

    Login - Signup Implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,7 @@
 import { Button } from '@/components/ui/button'
 
-export default function Home() {
-    return (
-        <div className="p-4">
-            <Button size="default" variant="destructive">
-                Click Me
-            </Button>
-        </div>
-    )
+const SetupPage = () => {
+    return <div className="p-4">This is a protected route !</div>
 }
+
+export default SetupPage
