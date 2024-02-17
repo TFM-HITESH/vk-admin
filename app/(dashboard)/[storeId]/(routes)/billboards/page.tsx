9535a619fc4409e1da3cc8e9ab4f08cbdddd0141
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-02-18 01:08:32 +0530

    Admin Data Table

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,4 +1,4 @@
-// import { format } from 'date-fns'
+import { format } from 'date-fns'
 
 import prismadb from '@/lib/prismadb'
 
@@ -15,17 +15,16 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
         },
     })
 
-    // const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
-    //     id: item.id,
-    //     label: item.label,
-    //     createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-    // }))
+    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+    }))
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                {/* <BillboardClient data={formattedBillboards} /> */}
-                <BillboardClient data={[]} />
+                <BillboardClient data={formattedBillboards} />
             </div>
         </div>
     )
