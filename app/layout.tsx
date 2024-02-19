1e7247b771fadc814724e70b0dda2ad41f62befe
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-02-20 00:11:48 +0530

    Favicon and Title Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ToastProvider } from '@/providers/toast-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-    title: 'VK Admin Dashboard',
+    title: 'Vicuna - Admin',
     description: 'VK Admin Dashboard',
 }
 
